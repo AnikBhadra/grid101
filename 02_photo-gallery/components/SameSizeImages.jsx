dc1d6929e5583a0a4b9b4ab980526a31d764166b
anik <anikbhadra01@gmail.com>
2022-06-06 18:35:17 +0530

    02_photo-gallery: SameSizeImages using map function

--- 02_photo-gallery/components/SameSizeImages.jsx
+++ 02_photo-gallery/components/SameSizeImages.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import '../styles/SameSizeImages.css'
-import images from '../assets/img'
-
+import images from '../assets/img.js'
 function SameSizeImages() {
     return (
-        <div>
-            <div className='container'>
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="../assets/food.jpg" alt="" />
-                <img src="https://images.unsplash.com/photo-1649562561948-94147ff3aadf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
-            </div>
+        <div className='container'>
+            {images.map((image) => {
+                return <img key={image.id} src={image.url} alt={image.alt} />
+            })}
+
         </div>
     )
 }
