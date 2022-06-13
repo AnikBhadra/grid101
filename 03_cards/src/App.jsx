8be7eac8395c31b935286ead199e922e9dfe381b
anik <anikbhadra01@gmail.com>
2022-06-13 19:27:51 +0530

    03_cards: responsive align/justify start

--- 03_cards/src/App.jsx
+++ 03_cards/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import ResponsiveAlignmentStretch from './components/ResponsiveAlignmentStretch'
+import ResponsiveAlign_JustifyStart from './components/ResponsiveAlign_JustifyStart'
 
 function App() {
 
   return (
     <div >
-      <ResponsiveAlignmentStretch />
+      <ResponsiveAlign_JustifyStart />
     </div>
   )
 }
